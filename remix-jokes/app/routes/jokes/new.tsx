204f19d901c6b500077bc8202ac8de956aa6b8ca
fahmi ahmad <fahmiahmad@tutanota.com>
2022-02-05 16:54:59 +0700

    cookie session

--- remix-jokes/app/routes/jokes/new.tsx
+++ remix-jokes/app/routes/jokes/new.tsx
@@ -1,6 +1,7 @@
 import type { ActionFunction } from "remix";
 import { redirect, useActionData, json } from "remix";
 import { db } from "~/utils/db.server";
+import { requireUserId } from "~/utils/session.server";
 
 function validateJokeContent(content: string) {
   if (content.length < 10) {
@@ -29,6 +30,7 @@ type ActionData = {
 const badRequest = (data: ActionData) => json(data, { status: 400 });
 
 export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
@@ -48,7 +50,9 @@ export const action: ActionFunction = async ({ request }) => {
   if (Object.values(fieldErrors).some(Boolean)) {
     return badRequest({ fieldErrors, fields });
   }
-  const joke = await db.joke.create({ data: fields });
+  const joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId },
+  });
   return redirect(`/jokes/${joke.id}`);
 };
 
