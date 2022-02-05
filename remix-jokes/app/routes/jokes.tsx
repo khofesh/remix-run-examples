ee06dceeef48e3d592dda5b76f0fb98dbe8f7167
fahmi ahmad <fahmiahmad@tutanota.com>
2022-02-05 10:33:37 +0700

    styling

---
+++ remix-jokes/app/routes/jokes.tsx
@@ -0,0 +1,24 @@
+import type { LinksFunction } from "remix";
+import { Outlet } from "remix";
+
+import stylesUrl from "../styles/jokes.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
+
+export default function JokesRoute() {
+  return (
+    <div>
+      <h1>J🤪KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
